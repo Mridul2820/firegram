b0d042ecc98183c9c4e0541bbc1f0b88a12a9f56
Mridul2820 <immridulpanda2820@gmail.com>
2021-01-30 01:42:01 +0530

    upload img progressbar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import UseStorage from '../hooks/UseStorage'
+
+const ProgressBar = ({ file, setFile }) => {
+    const {url, progress} = UseStorage(file)
+
+    useEffect(() => {
+        if(url) {
+            setFile(null)
+        }
+    },[url, setFile])
+
+    return (
+        <div className="progress-bar" style={{width: progress + '%'}} ></div>
+    )
+}
+
+export default ProgressBar

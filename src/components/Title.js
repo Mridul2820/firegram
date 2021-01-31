b2a2bb3579f1a22c6328d618baab7d6cd97129ff
Mridul <43516008+Mridul2820@users.noreply.github.com>
2021-01-31 16:37:25 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 const Title = () => {
     return (
         <div className="title">
-            <h1>FireGram</h1>
+            <h1>FireGram <br/>By Mridul</h1>
             <h2>Gallery</h2>
             <p>Photos Uploaded Till Now</p>
         </div>
